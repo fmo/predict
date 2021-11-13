1c2026e23f0b7f43b554fb1164248e7da21b199d
Mustafa Özyurt <mustafa.ozyurt@gmail.com>
2021-11-13 11:13:58 +0100

    Should not be possible to add text

--- app.js
+++ app.js
@@ -78,8 +78,8 @@ function populateGames(){
         <div id="game-${game.id}" class="form-group fs-2 justify-content-center row mt-2 align-items-center shadow p-3 mb-5 bg-body rounded">
             <div class="col-4 text-end">${game.homeTeam}</div>
             <div class="input-group input-group-sm w-25">
-                <input type="text" value="${homeTeamVal}" class="form-control home-team"> 
-                <input type="text" value="${awayTeamVal}" class="form-control away-team"> 
+                <input type="number" value="${homeTeamVal}" class="form-control home-team"> 
+                <input type="number" value="${awayTeamVal}" class="form-control away-team"> 
             </div>
             <div class="col-4 text-start">${game.awayTeam}</div>
         </div>`;
