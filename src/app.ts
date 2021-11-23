8b6606af06c120286208b1878761253c568b11ec
Mustafa Özyurt <mustafa.ozyurt@gmail.com>
2021-11-23 13:58:49 +0100

    Add Ts support

---
+++ src/app.ts
@@ -0,0 +1,84 @@
+import { games } from './games';
+import { ui } from './ui';
+import "../style/style.scss"
+
+window.addEventListener('load', populateGames);
+
+document.querySelector('.games').addEventListener('focusout', saveTheGuess);
+
+function validGuess(guess: any) {
+    if (guess < 0 ) {
+        return false;
+    }
+
+    const re = /^[0-9]{1,2}$/;
+
+    if (!re.test(guess) || guess > 15) {
+        return false;
+    }
+
+    return true;
+}
+
+function updateGames(guess: any, whichTeam: any, id: any) {
+    let guessedGames: any = [];
+    
+    let allGames = JSON.parse(localStorage.getItem('games'));
+
+    allGames.forEach(function(game: any) {
+        if (game.id === id) {
+            if (whichTeam === 'home-team') {
+                game = { 
+                    ...game, 
+                    "homeTeamGuess": guess, 
+                }
+            } else {
+                game = { 
+                    ...game, 
+                    "awayTeamGuess": guess, 
+                }
+            }
+        }
+        
+        guessedGames.push(game);
+    });
+
+    return guessedGames;
+}
+
+
+function saveTheGuess(e: any) {
+    const id = ui.getGameId(e.target.parentNode.parentNode.id);
+    const guess = e.target.value;
+
+    if (!validGuess(guess)) {
+        e.target.classList.add('is-invalid');
+        return;
+    }
+
+    let guessedGames = [];
+
+    if (e.target.classList.contains('home-team')){
+        guessedGames = updateGames(guess, 'home-team', id);
+    }
+
+    if (e.target.classList.contains('away-team')){
+        guessedGames = updateGames(guess, 'away-team', id);
+    }
+
+    localStorage.setItem('games', JSON.stringify(guessedGames));
+
+    populateGames();
+}
+
+function populateGames() {
+    if (!localStorage.getItem('games')) {
+        localStorage.setItem('games', JSON.stringify(games))
+    }
+
+    let allGames = JSON.parse(localStorage.getItem('games'));
+
+    let html = ui.populateGames(allGames);
+
+    document.querySelector('.games').innerHTML = html;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,24 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/app.ts',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
   },
   module: {
     rules: [
       {
         test: /\.s[ac]ss$/i,
         use: ["style-loader", "css-loader", "sass-loader",],
+      },
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
       },
     ],
   },
