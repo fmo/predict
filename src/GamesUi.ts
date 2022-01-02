4eeb543b5a4aca36597bc148287f342dfd5a6029
fmo <mustafa.ozyurt@gmail.com>
2022-01-02 13:13:16 +0300

    Move all dom stuff from ui to gamesUi

--- src/GamesUi.ts
+++ src/GamesUi.ts
@@ -1,7 +1,10 @@
 import { util } from './util';
+import { GameProps } from './Game';
 
 export class GamesUi {
-    constructor() {}
+    constructor() {
+        this.createScoreDiv = this.createScoreDiv.bind(this);
+    }
 
     createDateElement() {
         const parentGamesDiv = document.querySelector('.games');
@@ -17,5 +20,62 @@ export class GamesUi {
         divElement.appendChild(document.createTextNode(text));
 
         return divElement;
+    }
+
+    createTeamInput(guess: string, team: string) {
+        let homeTeamInput = document.createElement('input');
+        homeTeamInput.type = 'text';
+        homeTeamInput.value = guess;
+        homeTeamInput.className = team;
+
+        return homeTeamInput;
+    }
+
+    createScoreDiv(homeTeamGuess: string, awayTeamGuess: string) {
+        let scoreDiv = document.createElement('div');
+        scoreDiv.className = 'score';
+
+        const homeTeamInput = this.createTeamInput(homeTeamGuess, 'home-team');
+        scoreDiv.appendChild(homeTeamInput);
+
+        let spanElement = document.createElement('span');
+        let spanText = document.createTextNode(' - ');
+        spanElement.appendChild(spanText);
+
+        scoreDiv.appendChild(spanElement);
+
+        const awayTeamInput = this.createTeamInput(awayTeamGuess, 'away-team');
+        scoreDiv.appendChild(awayTeamInput);
+
+        return scoreDiv;
+    }
+
+    createNewGame(game: GameProps) {
+        let newGameDiv = document.createElement('div');
+        newGameDiv.id = `game-${game.id}`;
+        newGameDiv.className = 'game';
+
+        const teamsLabel = this.createDivWithText(
+            'teams-label',
+            `${game.homeTeam} - ${game.awayTeam}`
+        );
+
+        const gameTime = this.createDivWithText('game-time', game.gameTime);
+
+        const scoreDiv = this.createScoreDiv(
+            `${game.homeTeamGuess}`,
+            `${game.awayTeamGuess}`
+        );
+
+        newGameDiv.appendChild(teamsLabel);
+        newGameDiv.appendChild(gameTime);
+        newGameDiv.appendChild(scoreDiv);
+
+        return newGameDiv;
+    }
+
+    generateGames(newGame: any) {
+        const parentGamesDiv = document.querySelector('.games');
+        parentGamesDiv.insertAdjacentElement('beforeend', newGame);
     }
 }
--- src/ui.ts
+++ src/ui.ts
@@ -18,80 +18,14 @@ export class UI {
         return parseInt(gameIdArr[1]);
     }
 
-    createDivWithText(className: string, text: string) {
-        const divElement = document.createElement('div');
-        divElement.className = className;
-
-        divElement.appendChild(document.createTextNode(text));
-
-        return divElement;
-    }
-
-    createTeamInput(guess: string, team: string) {
-        let homeTeamInput = document.createElement('input');
-        homeTeamInput.type = 'text';
-        homeTeamInput.value = guess;
-        homeTeamInput.className = team;
-
-        return homeTeamInput;
-    }
-
-    createScoreDiv(homeTeamGuess: string, awayTeamGuess: string) {
-        let scoreDiv = document.createElement('div');
-        scoreDiv.className = 'score';
-
-        const homeTeamInput = this.createTeamInput(homeTeamGuess, 'home-team');
-        scoreDiv.appendChild(homeTeamInput);
-
-        let spanElement = document.createElement('span');
-        let spanText = document.createTextNode(' - ');
-        spanElement.appendChild(spanText);
-
-        scoreDiv.appendChild(spanElement);
-
-        const awayTeamInput = this.createTeamInput(awayTeamGuess, 'away-team');
-        scoreDiv.appendChild(awayTeamInput);
-
-        return scoreDiv;
-    }
-
-    createNewGame(game: GameProps) {
-        let newGameDiv = document.createElement('div');
-        newGameDiv.id = `game-${game.id}`;
-        newGameDiv.className = 'game';
-
-        const teamsLabel = this.createDivWithText(
-            'teams-label',
-            `${game.homeTeam} - ${game.awayTeam}`
-        );
-
-        const gameTime = this.createDivWithText('game-time', game.gameTime);
-
-        const scoreDiv = this.createScoreDiv(
-            `${game.homeTeamGuess}`,
-            `${game.awayTeamGuess}`
-        );
-
-        newGameDiv.appendChild(teamsLabel);
-        newGameDiv.appendChild(gameTime);
-        newGameDiv.appendChild(scoreDiv);
-
-        return newGameDiv;
-    }
-
-    generateGames(newGame: any) {
-        const parentGamesDiv = document.querySelector('.games');
-        parentGamesDiv.insertAdjacentElement('beforeend', newGame);
-    }
-
     populateGames() {
         const allGames = this.gameService.getAllGames;
 
         this.gamesUi.createDateElement();
 
         allGames.forEach((game: GameProps) => {
-            const newGame = this.createNewGame(game);
-            this.generateGames(newGame);
+            const newGame = this.gamesUi.createNewGame(game);
+            this.gamesUi.generateGames(newGame);
         });
     }
 
